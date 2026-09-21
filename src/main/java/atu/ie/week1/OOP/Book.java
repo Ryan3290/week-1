7bc7cce1cd38ec5dac194fb2d1577c761cb0e0ab
RyanFadden-STUDENT <G00476583@atu.ie>
2026-09-21 12:58:23 +0100

    added new method to show details

--- src/main/java/atu/ie/week1/OOP/Book.java
+++ src/main/java/atu/ie/week1/OOP/Book.java
@@ -6,4 +6,11 @@ public class Book {
     public int pageCount;
     public boolean available = true;
 
+    public void displayDetails() {
+        System.out.println("book:" + title);
+        System.out.println("book:" + author);
+        System.out.println("book:" + pageCount);
+        System.out.println("Is this book available?" + available);
+    }
+
 }
--- src/main/java/atu/ie/week1/OOP/Main.java
+++ src/main/java/atu/ie/week1/OOP/Main.java
@@ -6,6 +6,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello oop 1");
 
+        Book firstbook = new Book();
 
+        firstbook.title = "Dune";
+        firstbook.author = "Frank";
+        firstbook.pageCount = 412;
+
+        System.out.println("First book:" + firstbook.title);
+        System.out.println("First book:" + firstbook.author);
+        System.out.println("First book:" + firstbook.pageCount);
+        System.out.println("Is this book available?" + firstbook.available);
+        System.out.println("\n");
+
+        Book secondbook = new Book();
+        secondbook.title = "Star Trek";
+        secondbook.author = "Dave";
+        secondbook.pageCount = 300;
+        secondbook.available = true;
+
+        secondbook.displayDetails();
     }
 }
