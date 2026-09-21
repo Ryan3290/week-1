c0f0d11582d8ca874fc68d6b645d08a1a3963b66
RyanFadden-STUDENT <G00476583@atu.ie>
2026-09-21 12:30:11 +0100

    week 1

--- src/main/java/atu/ie/week1/OOP/Main.java
+++ src/main/java/atu/ie/week1/OOP/Main.java
@@ -4,6 +4,6 @@ package atu.ie.week1.OOP;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello oop");
+        System.out.println("hello oop 1");
     }
 }
